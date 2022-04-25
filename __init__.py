88f3f4710f8e91cdd7fc3579268616ce639242d6
Raimon Esteve <raimonesteve@gmail.com>
2022-04-25 11:43:51 +0200

    Allow add shops in product (multicompany) #048340

--- __init__.py
+++ __init__.py
@@ -2,10 +2,12 @@
 # copyright notices and license terms.
 from trytond.pool import Pool
 from . import product
+from . import shop
 
 
 def register():
     Pool.register(
         product.Template,
         product.TemplateSaleShop,
+        shop.SaleShop,
         module='product_shop', type_='model')
